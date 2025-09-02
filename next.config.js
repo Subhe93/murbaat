8dc8b7549e7f9ea586732c83c204097a783be6ec
subhehmoden <hmed2sbhe@gmail.com>
2025-09-02 13:46:21 +0300

    upload

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ const nextConfig = {
       "morabaat.com",
       "cdn.morabaat.com",
       "images.example.com",
+      "murabaat.iwings-digital.com",
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
